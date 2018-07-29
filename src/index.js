9469c9a9dd6ff2d3c07a19c29ff2fe4cbfab5dae
Eric Pham <ericpham380@gmail.com>
2018-07-29 10:27:16 -0700

    Loading images and fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import webpackIcon from './webpack.png';
 
 function component() {
   var element = document.createElement('div');
@@ -8,6 +9,11 @@ function component() {
   // Lodash is now imported from line 1
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existin div
+  var icon = new Image();
+  icon.src = webpackIcon;
+  element.appendChild(icon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,25 @@ module.exports = {
   module: {
     rules: [
       {
+        // Loading CSS using css-loader and style-loader
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
+        ]
+      },
+      {
+        // Loading images using file-loader
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        // Loading fonts using file-loader
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
